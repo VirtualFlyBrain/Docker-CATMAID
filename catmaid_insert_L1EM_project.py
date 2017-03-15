b648eb4d95867a8cc1343ece2c044e5203b9b2a4
Rob Court <robbie.court@gmail.com>
2017-03-15 14:45:27 +0000

    removing demo projects

--- catmaid_insert_L1EM_project.py
+++ catmaid_insert_L1EM_project.py
@@ -32,6 +32,11 @@ class Command(NoArgsCommand):
              'comment': '''<p></p>''',
              'trakem2_project': False})
 
+        # Remove example Projects:
+        Project.objects.exclude(title='Default Project')
+        Project.objects.exclude(title='Evaluation data set')
+        Project.objects.exclude(title='Focussed Ion Beam (FIB)')
+        
         # Make sure that project and its stacks exist, and are
         # linked via ProjectStack:
 
