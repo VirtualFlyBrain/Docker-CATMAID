2ab2c0507658c89066f7badc9d41575b0e8d5d82
Rob Court <robbie.court@gmail.com>
2017-03-15 12:23:16 +0000

    merging

--- init.sh
+++ init.sh
@@ -6,11 +6,7 @@ service postgresql start
 # Load tile data
 ln -s /opt/VFB/L1EM /opt/tiles
 cd /opt/VFB/ 
-tftp $TFTP-SERVER << fin
-  get /catmaid/L1EM.tar.gz 
-  quit
-fin
-tar -zxvf L1EM.tar.gz &
+tftp ${TFTP-SERVER} << fin; get /catmaid/L1EM.tar.gz; quit; fin; tar -zxvf L1EM.tar.gz &
 
 # start CATMAID
 supervisord -n
