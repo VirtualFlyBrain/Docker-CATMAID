ae1134a8e5e98dd7ec9a70b0239686cb0e624e50
Rob Court <robbie.court@gmail.com>
2017-03-15 11:59:00 +0000

    server setting

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ service postgresql start
 # Load tile data
 ln -s /opt/VFB/L1EM /opt/tiles
 cd /opt/VFB/ 
-tftp vfbds0.inf.ed.ac.uk << fin
+tftp $TFTP-SERVER << fin
   get /catmaid/L1EM.tar.gz 
   quit
 fin && \
