1f97da95b67136acbb64c454a669339027810278
Rob Court <robbie.court@gmail.com>
2017-03-16 14:20:14 +0000

    pulling files directly

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ service postgresql start
 # Load tile data
 ln -s /opt/VFB/L1EM /opt/tiles
 cd /opt/VFB/ 
-echo 'get /catmaid/L1EM.tar.gz' | tftp ${FILESERVER} && tar -zxvf L1EM.tar.gz &
+echo 'get /catmaid/files.txt' | tftp "${FILESERVER}" && /opt/VFB/tftpbatch.sh /opt/VFB/files.txt "${FILESERVER}" &
 
 # start CATMAID
 supervisord -n
