d6cac4c36c0a9a9856919893b46e8bc2560ecaf2
Rob Court <robbie.court@gmail.com>
2017-03-17 10:40:15 +0000

    only z0 zoom 6

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ service postgresql start
 # Load tile data
 ln -s /opt/VFB/L1EM /opt/tiles
 cd /opt/VFB/ 
-echo 'get /catmaid/files.txt' | tftp "${FILESERVER}" && cat files.txt | grep small.jpg > /opt/VFB/small.txt && /opt/VFB/tftpbatch.sh /opt/VFB/small.txt "${FILESERVER}" && cat files.txt | grep 111-8/[0-9]*/0/ > /opt/VFB/images.txt && /opt/VFB/tftpbatch.sh /opt/VFB/images.txt "${FILESERVER}" &
+echo 'get /catmaid/files.txt' | tftp "${FILESERVER}" && cat files.txt | grep small.jpg > /opt/VFB/small.txt && /opt/VFB/tftpbatch.sh /opt/VFB/small.txt "${FILESERVER}" && cat files.txt | grep 111-8/0/6/ > /opt/VFB/images.txt && /opt/VFB/tftpbatch.sh /opt/VFB/images.txt "${FILESERVER}" &
 
 # start CATMAID
 supervisord -n
