ef1f170fbee7c7bc84aceca24579bd47ebc37733
Rob Court <robbie.court@gmail.com>
2018-03-01 15:55:18 +0000

    using script

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /backup/archive/
 mv --backup=t /backup/catmaid_dum*.sql /backup/archive/
-pg_dump --clean -T treenode_edge -U catmaid_user catmaid -f /backup/catmaid_dump.sql
+python /home/scripts/database/backup-database.py /backup/
 
