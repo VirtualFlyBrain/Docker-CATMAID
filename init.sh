8a721d612c6a23a2134a3956fd7fb0d799db1983
Rob Court <robbie.court@gmail.com>
2018-03-01 16:56:43 +0000

    typo fix

--- init.sh
+++ init.sh
@@ -5,7 +5,7 @@ service postgresql start
 
 # Check for recovery DB
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
-  python /home/scripts/database/revert-database.py /backup/catmaid_FAFB_2018-03-01T15-47-11.bz2 /backup/*.bz2
+  python /home/scripts/database/revert-database.py /backup/*.bz2
 fi
 
 # start CATMAID
