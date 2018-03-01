2e42ec627500fa75d3a73353e5f7f09bf469e520
Rob Court <robbie.court@gmail.com>
2018-03-01 11:30:26 +0000

    checking for DB restore

--- init.sh
+++ init.sh
@@ -3,5 +3,10 @@
 # Start DB early 
 service postgresql start
 
+# Check for recovery DB
+if [ $(ls /backup/catmaid_dum*.sql | wc -l) -eq 1 ]; then
+  psql -U catmaid_user -d catmaid -f /backup/catmaid_dum*.sql
+fi
+
 # start CATMAID
 /home/scripts/docker/catmaid-entry.sh standalone
