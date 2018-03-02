2a0c12f6ef944f1d15f8795eef46bd793bc4c3f5
Rob Court <robbie.court@gmail.com>
2018-03-02 12:41:33 +0000

    typo fix

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir -p /backup/archive/
-mv --backup=t /backup/catmaid_dum*.sql /backup/archive/
+mv --backup=t /backup/*.bz2 /backup/archive/
 python /home/scripts/database/backup-database.py /backup/
 
