fe424614096e58127a5596da096751454012cd1c
Rob Court <robbie.court@gmail.com>
2018-08-01 15:22:35 +0100

    restore migrate after backup restore

--- init.sh
+++ init.sh
@@ -6,20 +6,22 @@ service postgresql start
 # start CATMAID
 /home/scripts/docker/catmaid-entry.sh standalone &
 
-# migrate DB
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-workon catmaid
-cd /home/django/projects
-python manage.py makemigrations catmaid
-python manage.py migrate
-
 echo 'Start of Service' >> /var/log/postgresql/postgresql-10-main.log
 echo 'Start of Service' >> /var/log/nginx/error.log
 echo 'Start of Service' >> /var/log/nginx/access.log
 
 # Check for recovery DB
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
-  sleep 1m && python /home/scripts/database/revert-database.py /backup/*.bz2 && sleep 1m && python /home/scripts/database/revert-database.py /backup/*.bz2 &
+  sleep 1m
+  python /home/scripts/database/revert-database.py /backup/*.bz2
+  sleep 1m
+  python /home/scripts/database/revert-database.py /backup/*.bz2
+  # migrate DB
+  source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+  workon catmaid
+  cd /home/django/projects
+  python manage.py makemigrations catmaid
+  python manage.py migrate
 fi
 
 tail -F /var/log/nginx/error.log >&2 &
