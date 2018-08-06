c523a8e72eb1ebab2df5714045d7a8d29f02ed98
Rob Court <robbie.court@gmail.com>
2018-08-06 11:43:05 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,20 +10,20 @@ echo 'Start of Service' >> /var/log/postgresql/postgresql-10-main.log
 echo 'Start of Service' >> /var/log/nginx/error.log
 echo 'Start of Service' >> /var/log/nginx/access.log
 
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+workon catmaid
+cd /home/django/projects
+
 # Check for recovery DB
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
   sleep 1m
   python /home/scripts/database/revert-database.py /backup/*.bz2
   sleep 1m
   python /home/scripts/database/revert-database.py /backup/*.bz2
-  # migrate DB
-  source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-  workon catmaid
-  cd /home/django/projects
-  python manage.py catmaid_rebuild_edge_table
-  python manage.py makemigrations catmaid
-  python manage.py migrate
 fi
+
+# set Admin Password
+cat /home/scripts/docker/modify_superuser.py | python manage.py shell
 
 tail -F /var/log/nginx/error.log >&2 &
 tail -F /var/log/nginx/access.log &
