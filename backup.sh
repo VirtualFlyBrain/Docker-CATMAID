acdaee4e4c11f1cd71710ced36b11729f076eea1
Rob Court <robbie.court@gmail.com>
2018-08-06 11:30:17 +0100

    adding virtual env

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+workon catmaid
+cd /home/django/projects
 mkdir -p /backup/archive/
 mv --backup=t /backup/*.bz2 /backup/archive/
 python /home/scripts/database/backup-database.py /backup/
