a4806f511b50d42b29ca75b0799f89d5af11767e
Rob Court <rcourt@ed.ac.uk>
2018-08-06 18:07:28 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ cd /home/django/projects
 # Check for recovery DB
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
   sleep 20s
-  bunzip2 -c /backup/*.bz2 | pg_restore --create -U postgres --no-password -d ${DB_NAME}
+  bunzip2 -c /backup/*.bz2 | pg_restore --create --clean -U postgres --no-password -d postgres
   sleep 1m
 fi
 
