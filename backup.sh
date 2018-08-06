abc40e7a236ec96070b516bde6c778f9d6da126c
Rob Court <rcourt@ed.ac.uk>
2018-08-06 16:29:42 +0100

    adding Fc

--- backup.sh
+++ backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p /backup/archive/
 mv --backup=t /backup/*.bz2 /backup/archive/
-pg_dump --clean -U postgres --no-password catmaid | bzip2 > /backup/VFB-CATMAID-BACKUP_$(date +'%Y-%m-%dT%H_%M_%S')-C${HOSTNAME}.bz2
+pg_dump -Fc --clean -U postgres --no-password catmaid | bzip2 > /backup/VFB-CATMAID-BACKUP_$(date +'%Y-%m-%dT%H_%M_%S')-C${HOSTNAME}.bz2
