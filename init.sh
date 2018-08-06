475f0841db537b1a5bc4e26a8b9f566ae31059e3
Rob Court <rcourt@ed.ac.uk>
2018-08-06 16:29:16 +0100

    swapping to pg_restore

--- init.sh
+++ init.sh
@@ -13,7 +13,8 @@ cd /home/django/projects
 # Check for recovery DB
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
   sleep 20s
-  bunzip2 -c /backup/*.bz2 | psql -U postgres --no-password ${DB_NAME}
+  bunzip2 -c /backup/*.bz2 | pg_restore -U postgres --no-password
+  sleep 1m
 fi
 
 # start CATMAID
