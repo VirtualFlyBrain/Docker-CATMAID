6a0f5186a85f681a545bed98a0035a559ade7260
Rob Court <rcourt@ed.ac.uk>
2018-08-06 15:53:13 +0100

    manual direct backup

--- backup.sh
+++ backup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-workon catmaid
-cd /home/django/projects
 mkdir -p /backup/archive/
 mv --backup=t /backup/*.bz2 /backup/archive/
-python /home/scripts/database/backup-database.py /backup/
-
+pg_dump --clean -U postgres --no-password catmaid | bzip2 > /backup/VFB-CATMAID-BACKUP_$(date +'%Y-%m-%dT%H_%M_%S')-C${HOSTNAME}.bz2
