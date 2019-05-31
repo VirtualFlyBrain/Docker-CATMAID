89ec57d722edb59e0f1481a73065f90beeb8cb6d
Rob Court <rcourt@ed.ac.uk>
2019-05-31 13:40:15 +0100

    restoring original cmd

--- init.sh
+++ init.sh
@@ -14,7 +14,7 @@ cd /home/django/projects
 if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
   sleep 20s
   psql -U postgres --no-password -c "CREATE USER ${DB_USER} WITH CREATEDB CREATEROLE SUPERUSER PASSWORD '${DB_PASS}';"
-  bunzip2 -c /backup/*.bz2 | pg_restore --create --clean -U ${DB_USER} --no-password -d ${DB_NAME}
+  bunzip2 -c /backup/*.bz2 | pg_restore --create --clean -U postgres --no-password -d postgres
   sleep 20s
   manage.py catmaid_rebuild_edge_table
 fi
