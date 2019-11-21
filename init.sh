e840850673f96946faa77fa76cf3e50a9331a00f
Rob Court <rcourt@ed.ac.uk>
2019-11-21 13:35:48 +0000

    stripping admin password

--- init.sh
+++ init.sh
@@ -29,9 +29,6 @@ echo 'Start of Service' >> /var/log/postgresql/postgresql-10-main.log
 echo 'Start of Service' >> /var/log/nginx/error.log
 echo 'Start of Service' >> /var/log/nginx/access.log
 
-# set Admin Password
-cat /opt/VFB/modify_superuser.py | python manage.py shell
-
 tail -F /var/log/nginx/error.log >&2 &
 tail -F /var/log/nginx/access.log &
 
