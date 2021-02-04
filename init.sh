6b5672a698275956ce3d1894729ad25ba766a540
Rob Court <rcourt@ed.ac.uk>
2021-02-04 14:36:15 +0000

    typo fix

--- init.sh
+++ init.sh
@@ -16,7 +16,7 @@ if [ $(ls /backup/*.bz2 | wc -l) -eq 1 ]; then
   psql -U postgres --no-password -c "CREATE USER ${DB_USER} WITH CREATEDB CREATEROLE SUPERUSER PASSWORD '${DB_PASS}';"
   bunzip2 -c /backup/*.bz2 | pg_restore --create --clean -U postgres --no-password -d postgres
   sleep 20s
-  manage.py catmaid_rebuild_edge_table
+  python manage.py catmaid_rebuild_edge_table
 fi
 
 # start CATMAID
