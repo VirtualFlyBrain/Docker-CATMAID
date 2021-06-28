848d07476b9b6a137c56591304f88bc1eb52afd2
Rob Court <rcourt@ed.ac.uk>
2021-06-28 18:18:26 +0100

    loading DB

--- init.sh
+++ init.sh
@@ -13,7 +13,12 @@ cat /opt/VFB/modify_superuser.py | python manage.py shell
 
 tail -F /var/log/nginx/error.log >&2 &
 tail -F /var/log/nginx/access.log &
+tail -F /var/log/postgresql/postgresql-11-main.log &
 
-/bin/bash /home/scripts/docker/catmaid-entry.sh standalone & 
-
-tail -F /var/log/postgresql/postgresql-11-main.log
+if [ -e /backup/*.pgsql ]
+then
+  export DB_FIXTURE=true
+  cat /backup/*.pgsql | /bin/bash /home/scripts/docker/catmaid-entry.sh standalone
+else
+  /bin/bash /home/scripts/docker/catmaid-entry.sh standalone
+fi
