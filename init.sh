40aaeae19cee8984338c5ac8d01d3fd51b49bde7
Rob Court <rcourt@ed.ac.uk>
2021-06-29 18:06:10 +0100

    removing forced password set

--- init.sh
+++ init.sh
@@ -4,12 +4,6 @@ echo 'Start of Service' >> /var/log/postgresql/postgresql-11-main.log
 echo 'Start of Service' >> /var/log/nginx/error.log
 echo 'Start of Service' >> /var/log/nginx/access.log
 
-# set Admin Password
-sleep 2m && \
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh && \
-workon catmaid && \
-cd /home/django/projectscat /opt/VFB/modify_superuser.py | python manage.py shell &
-
 tail -F /var/log/nginx/error.log >&2 &
 tail -F /var/log/nginx/access.log &
 tail -F /var/log/postgresql/postgresql-11-main.log &
