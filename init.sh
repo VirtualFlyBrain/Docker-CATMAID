c8c59d85a2bb35da798ee260c7f22b094da37f16
Rob Court <rcourt@ed.ac.uk>
2021-06-29 14:59:46 +0100

    cleaning up order

--- init.sh
+++ init.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
-workon catmaid
-cd /home/django/projects
-
 echo 'Start of Service' >> /var/log/postgresql/postgresql-11-main.log
 echo 'Start of Service' >> /var/log/nginx/error.log
 echo 'Start of Service' >> /var/log/nginx/access.log
 
 # set Admin Password
-cat /opt/VFB/modify_superuser.py | python manage.py shell
+sleep 2m && \
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh && \
+workon catmaid && \
+cd /home/django/projectscat /opt/VFB/modify_superuser.py | python manage.py shell &
 
 tail -F /var/log/nginx/error.log >&2 &
 tail -F /var/log/nginx/access.log &
@@ -18,7 +17,7 @@ tail -F /var/log/postgresql/postgresql-11-main.log &
 if [ -e /backup/*.pgsql ]
 then
   export DB_FIXTURE=true
-  cat /backup/*.pgsql | /bin/bash /home/scripts/docker/catmaid-entry.sh standalone
+  cat /backup/*.pgsql | /home/scripts/docker/catmaid-entry.sh standalone
 else
   /bin/bash /home/scripts/docker/catmaid-entry.sh standalone
 fi
